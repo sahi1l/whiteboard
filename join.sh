bb753874b89649381ba6c59b424dc79a1f2e3f28
Sam Hill <physhill@gmail.com>
2023-01-28 16:45:14 -0500

    removed some old files

--- join.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-outfile=$1
-shift 1
-echo $outfile
-export PATH=$PATH:/Library/TeX/texbin
-echo "pdfjoin --outfile $outfile \"$@\" 2>/dev/null"
-
-pdfjoin --outfile $outfile "$@" 2>/dev/null
-open -a Preview $outfile
